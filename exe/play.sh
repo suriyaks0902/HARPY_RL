6b9472f9fa594aad2157009d67a234ca563ab68e
Suriya Kasiyalan Siva <90970879+suriyaks0902@users.noreply.github.com>
2025-07-27 13:57:32 -0400

    Add files via upload

---
+++ exe/play.sh
@@ -0,0 +1,2 @@
+# python ../scripts/play.py  --test_model 'versatile_walking' 
+python ../scripts/play.py  --test_model '/home/suriya/cassie_ws/src/cassie_rl_walking/ckpts/vnav_phase_thruster_v1_rnds1/model-4505576'
